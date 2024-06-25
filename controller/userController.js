4db4b3f7c7480046f3cd653a26c417697c2d8293
viveks5898 <singh.vivek5898@gmail.com>
2024-06-25 19:49:57 +0530

    new updates

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const db = require('../config/db');
 const transporter = require('../config/email');
 require('dotenv').config();
 
+
 exports.submitForm = (req, res) => {
     const {
         parent_name_prefix,
